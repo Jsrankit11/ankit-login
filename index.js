23cb6255da0c2dfc46cd908baac71f2a5cd9315c
Jsrankit11 <ankitchaudhary8081039@gmail.com>
2025-04-10 21:07:46 +0530

    Add files via upload
    ankit dost

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent the form from submitting
+
+//     // Get the values from the input fields
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     // Simple validation (you can replace this with actual authentication logic)
+//     if (username === 'admin' && password === 'password') {
+//         alert('Login successful!');
+//         // Redirect to another page or perform other actions
+//     } else {
+//         document.getElementById('error-message').innerText = 'Invalid username or password';
+//     }
+// });
